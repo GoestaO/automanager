83ba7849f58f6121028828a32c0486c05e040079
Goesta.O@gmail.com <Goesta.O@gmail.com@b964f150-8f02-8734-d305-2e62255f245e>
2013-05-08 14:12:52 +0000

    Zweiten Konstruktor erzeugt

--- trunk/automanager/src/automanager/Auto.java
+++ trunk/automanager/src/automanager/Auto.java
@@ -1,43 +1,43 @@
 package automanager;
+
 import java.io.Serializable;
 
-
-public class Auto implements Serializable{
+public class Auto implements Serializable {
 	private String hersteller;
 	private int leistung;
 	private int anzahlTueren;
 	private boolean hatAnhaengerkupplung;
-	
-	//Getter
+
+	// Getter
 	public String getHersteller() {
 		return hersteller;
 	}
-	
+
 	public int getLeistung() {
 		return leistung;
 	}
-	
+
 	public int getAnzahlTueren() {
 		return anzahlTueren;
 	}
-	
+
 	public boolean getHatAnhaengerkupplung() {
 		return hatAnhaengerkupplung;
 	}
-	
-	//Setter	
+
+	// Setter
 	public void setHersteller(String hersteller) {
-		this.hersteller = hersteller; 
+		this.hersteller = hersteller;
 	}
-	
+
 	public void setLeistung(int leistung) {
 		this.leistung = leistung;
 	}
-	
+
 	public void setAnzahlTueren(int anzahlTueren) {
 		this.anzahlTueren = anzahlTueren;
 	}
-	
+
 	public void setHatAnhaengerkupplung(boolean hatAnhaengerkupplung) {
 		this.hatAnhaengerkupplung = hatAnhaengerkupplung;
 	}
@@ -46,19 +46,17 @@ public class Auto implements Serializable{
 	public void tunen(int tuneWert) {
 		this.leistung = this.leistung + tuneWert;
 	}
-	
-	public Auto(String hersteller, int leistung, int anzahlTueren, boolean hatAnhaengerkupplung) {
+
+	public Auto(String hersteller, int leistung, int anzahlTueren) {
 		this.hersteller = hersteller;
 		this.leistung = leistung;
 		this.anzahlTueren = anzahlTueren;
+	}
+	
+	public Auto(String hersteller, int leistung, int anzahlTueren,
+			boolean hatAnhaengerkupplung) {
+		this(hersteller, leistung, anzahlTueren);
 		this.hatAnhaengerkupplung = hatAnhaengerkupplung;
 	}
 
-	
-
-	
-	
-
-	
 }
-
